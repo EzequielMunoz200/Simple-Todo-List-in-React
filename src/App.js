6ea3f2ad0a1aa604384ef0387d8539c0574e0f03
ezequiel <eemunoz.e@gmail.com>
2020-08-08 06:41:19 +0200

    Created component TaskRow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+
+  const [userName, setUserName] = useState('John Doe'); //owner
+  const [TaskItems, setTaskItems] = useState([
+    { name: 'Task number one', done: true },
+    { name: 'Task number two', done: false },
+    { name: 'Task number three', done: false },
+    { name: 'Task number four', done: true },
+    { name: 'Task number five', done: false },
+  ])
+
+  const taskTableRows = () => {
+    return TaskItems.map(task => (
+      <tr key={task.name}>
+        <td>{task.name}</td>
+      </tr>
+
+    ))
+  }
   return (
-    <div className="App">
+    <div >
+      <h1>Hello world!</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>
+          {taskTableRows()}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/components/TaskRow.js
@@ -0,0 +1,8 @@
+import React from 'react:';
+
+export const TaskRow = props => (
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+);
